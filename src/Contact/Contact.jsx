efe6c32e79c33e6328def6133316f29ec31d3bea
Lucas LAMOTHE <lucas.lamothe@epitech.eu>
2021-09-30 14:19:47 +0200

    [ADD] Contact form management + Sending email

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,14 +1,60 @@
 import { useForm } from "react-hook-form";
+import { useCookies } from 'react-cookie';
+import emailjs from "emailjs-com";
+import toastr from "toastr";
+
+import "./Contact.scss";
+
+const configEmailJS = {
+    serviceID: "service_kcvtrg2",
+    templateID: "template_202g84f",
+    userID: "user_EfMO9fN1EC3FMIiHzrjmU"
+}
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
 
 const Contact = _ => {
-    const {register, handleSubmit} = useForm({ shouldUseNativeValidation: true });
+    const { register, handleSubmit, reset } = useForm({ shouldUseNativeValidation: true });
+    const [ cookies, setCookie ] = useCookies(['emailSend']);
 
     const onSubmit = data => {
-        console.log(data);
+        const emailSendCount = parseInt(cookies.emailSend) || 0;
+
+        reset();
+        if (emailSendCount >= 5) {
+            toastr.info("Vous avez atteint la limite de demande de contact.");
+            return;
+        }
+        emailjs.send(configEmailJS.serviceID, configEmailJS.templateID, {
+            contact_email: data.email,
+            message: data.message
+        }, configEmailJS.userID).then(function(response) {
+            setCookie('emailSend', emailSendCount + 1);
+            toastr.success("Email de contact envoyé avec succès.");
+        }, function(error) {
+            console.log('FAILED...', error);
+            toastr.error("Erreur: Impossible d'envoyer l'email.");
+        });
     };
 
     return (
-        <div className="Contact">
+        <div id="contact" className="Contact">
             <div className="container mx-auto my-14">
                 <div className="Contact-title mb-16 text-center transition duration-1000 ease-in-out transform hover:scale-95">
                     <h2 className="text-4xl font-bold">Contact 📭</h2>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CookiesProvider } from 'react-cookie';
 
 import './index.css';
 
@@ -7,7 +8,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <CookiesProvider>
+          <App />
+      </CookiesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
